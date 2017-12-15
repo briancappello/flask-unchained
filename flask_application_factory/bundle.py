ba8a7d9337bd48610c014c217cc85e32e3ce9da0
Brian Cappello <briancappello@gmail.com>
2017-12-14 20:14:31 -0500

    pep8

--- flask_application_factory/bundle.py
+++ flask_application_factory/bundle.py
@@ -1,23 +1,30 @@
 import os
 import sys
 
+
 class Bundle:
     module_name = None  # type: str
-    root_dir = None  # type: str
     """Top-level module name of the bundle (dot notation)"""
+
+    root_dir = None  # type: str
+    """
+    Top-level folder path of the bundle (defaults to the module's folder if
+    not provieded)
+    """
 
     hooks = []
 
     def __init__(self):
         if not self.module_name:
-            raise AttributeError(
-                f'{self.__class__.__name__} is missing a `module_name` attribute')
+            raise AttributeError(f'{self.__class__.__name__} is missing a '
+                                 f'`module_name` attribute')
 
         if self.module_name.endswith('.bundle'):
             self.module_name = self.module_name[:-len('.bundle')]
 
         if not self.root_dir:
-            self.root_dir = os.path.dirname(sys.modules[self.module_name].__file__)
+            self.root_dir = os.path.dirname(
+                sys.modules[self.module_name].__file__)
 
     @property
     def name(self) -> str:
