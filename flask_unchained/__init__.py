bd3b62cf891eef5415df0e81138acc30d8cbb65c
Brian Cappello <briancappello@gmail.com>
2017-12-16 01:00:16 -0500

    alphabetize imports, export FlaskUnchained class

--- flask_unchained/__init__.py
+++ flask_unchained/__init__.py
@@ -1,7 +1,8 @@
-from .base_config import FlaskUnchainedConfig
-from .bundle import Bundle
 from .app_factory import AppFactory
 from .app_factory_hook import AppFactoryHook
+from .base_config import FlaskUnchainedConfig
+from .bundle import Bundle
+from .flask_unchained import FlaskUnchained
 from .utils import (
     camel_case,
     get_boolean_env,
