7b606320d11f15204f3ceab69c8acc65a51d9caf
Brian Cappello <briancappello@gmail.com>
2017-12-17 15:00:12 -0500

    export unchained extension

--- flask_unchained/__init__.py
+++ flask_unchained/__init__.py
@@ -2,6 +2,7 @@ from .app_factory import AppFactory
 from .app_factory_hook import AppFactoryHook
 from .base_config import AppConfig
 from .bundle import Bundle
+from .unchained import unchained
 from .utils import (
     camel_case,
     get_boolean_env,
