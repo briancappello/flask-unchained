873a31c7c10a540a730c53b52d3cc9385f32e504
Brian Cappello <briancappello@gmail.com>
2017-12-21 14:17:57 -0500

    update test_bundle

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -1,37 +1,36 @@
 from flask_unchained import Bundle
 
-from .fixtures.app_bundle import AppBundle
+from .fixtures.myapp import AppBundle
 from .fixtures.vendor_bundle import VendorBundle
 
 
 class FoobarBundle(Bundle):
-    pass
+    name = 'foobaz'
 
 
-def test_module_name_descriptor_works_automatically():
+class TestBundle:
+    def test_module_name_descriptor_works_automatically(self):
+        assert FoobarBundle.module_name == 'tests.test_bundle'
 
-    assert FoobarBundle.module_name == 'tests.test_bundle'
+    def test_it_strips_bundle_module_suffix_from_declared_module_name(self):
+        class FooBundle(Bundle):
+            module_name = 'tests.foo.bundle'
+        assert FooBundle.module_name == 'tests.foo'
 
+    def test_module_name_descriptor_strips_bundle_module_suffix(self):
+        assert VendorBundle.module_name == 'tests.fixtures.vendor_bundle'
 
-def test_it_strips_bundle_from_declared_module_name():
-    class FooBundle(Bundle):
-        module_name = 'tests.foo.bundle'
+    def test_declared_name_descriptor(self):
+        assert FoobarBundle.name == 'foobaz'
 
-    assert FooBundle.module_name == 'tests.foo'
+    def test_automatic_name_descriptor(self):
+        class AmazingBundle(Bundle):
+            pass
+        assert AmazingBundle.name == 'amazing_bundle'
 
+    def test_automatic_app_bundle_name_descriptor(self):
+        assert AppBundle.name == 'myapp'
 
-def test_module_name_descriptor_strips_bundle_name_automatically():
-    assert VendorBundle.module_name == 'tests.fixtures.vendor_bundle'
-
-
-def test_name_descriptor_works_automatically():
-    assert FoobarBundle.name == 'foobar_bundle'
-
-
-def test_name_descriptor_can_be_declared():
-    assert AppBundle.name == 'app'
-
-
-def test_repr():
-    assert str(FoobarBundle) == "class <Bundle name='foobar_bundle' " \
-                                              "module='tests.test_bundle'>"
+    def test_repr(self):
+        expected = "class <Bundle name='foobaz' module='tests.test_bundle'>"
+        assert str(FoobarBundle) == expected
