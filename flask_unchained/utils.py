f7eb19719ff00a7060e813846c510efcbe53ceb2
Brian Cappello <briancappello@gmail.com>
2018-03-28 21:41:22 -0400

    add ConfigProperty and ConfigPropertyMeta utilty classes for backwards-compatibility with extensions that do silly things

--- flask_unchained/utils.py
+++ flask_unchained/utils.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import re
 
+from flask import current_app
 from importlib import import_module
 
 _missing = object()
@@ -16,6 +17,51 @@ class AttrDict(dict):
 
     def __repr__(self):
         return f'AttrDict({super().__repr__()})'
+
+
+class ConfigProperty:
+    """
+    Used in conjunction with ConfigPropertyMeta, allows extension classes to
+    create properties that proxy to the config value, eg app.config.get(key)
+
+    If key is left unspecified, in will be injected by ConfigPropertyMeta,
+    defaulting to '{ext_class_name}_{property_name}'.upper()
+    """
+    def __init__(self, key=None):
+        self.key = key
+
+    def __get__(self, instance, cls):
+        return current_app.config.get(self.key)
+
+
+class ConfigPropertyMeta(type):
+    """
+    Use this metaclass to enable config properties on extension classes. I'm not
+    sold on this being a good idea for *new* extensions, but for backwards
+    compatibility with existing extensions that have silly __getattr__ magic, I
+    think it's a big improvement. (NOTE: this only works when the application
+    context is available, but that's no different than the behavior of what it's
+    meant to replace)
+
+    Example usage::
+
+        class MyExtension(metaclass=ConfigPropertyMeta):
+            __config_prefix__ = 'MY_EXTENSION'
+            # if __config_prefix__ is unspecified, default is class_name.upper()
+
+            foobar: Optional[FunctionType] = ConfigProperty()
+            _custom: Optional[str] = ConfigProperty('MY_EXTENSION_CUSTOM')
+
+        my_extension = MyExtension(app)
+        my_extension.foobar == current_app.config.get('MY_EXTENSION_FOOBAR')
+        my_extension._custom == current_app.config.get('MY_EXTENSION_CUSTOM')
+    """
+    def __init__(cls, class_name, bases, clsdict):
+        super().__init__(class_name, bases, clsdict)
+        config_prefix = clsdict.get('__config_prefix__', class_name).strip('_')
+        for property_name, descriptor in clsdict.items():
+            if isinstance(descriptor, ConfigProperty) and not descriptor.key:
+                descriptor.key = f'{config_prefix}_{property_name}'.upper()
 
 
 def deep_getattr(clsdict, bases, name, default=_missing):
