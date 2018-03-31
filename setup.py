3ddb9538062273fdc28fc189efd3d772a7069932
Brian Cappello <briancappello@gmail.com>
2018-03-31 01:10:34 -0400

    update desc

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='Flask Unchained',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='Flask Unchained',
+    description='The better way to build large Flask applications',
     long_description=long_description,
     url='https://github.com/briancappello/flask-unchained',
     author='Brian Cappello',
