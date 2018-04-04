952e783e1b6fd1caa05e41f50a32bda11c7cedd5
Brian Cappello <briancappello@gmail.com>
2018-04-03 20:35:24 -0400

    update development status classifier to alpha

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
