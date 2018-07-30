58923ebae1814d2c43eb341756f7cc8f76e785c2
Brian Cappello <briancappello@gmail.com>
2018-07-29 21:03:36 -0600

    include modified script.py.make from alembic so that the `flask db init` command works correctly

--- flask_unchained/bundles/sqlalchemy/extensions/migrate.py
+++ flask_unchained/bundles/sqlalchemy/extensions/migrate.py
@@ -1,16 +1,26 @@
-from flask import Flask
-from flask_migrate import Migrate as BaseMigrate
-from flask_unchained import unchained, injectable
+import os
+
+from flask_migrate import Migrate as BaseMigrate, Config
+from flask_unchained import FlaskUnchained, unchained, injectable
 
 from .sqlalchemy import SQLAlchemy
 
 
 class Migrate(BaseMigrate):
     @unchained.inject('db')
-    def init_app(self, app: Flask, db: SQLAlchemy = injectable):
+    def init_app(self, app: FlaskUnchained, db: SQLAlchemy = injectable):
         alembic_config = app.config.get('ALEMBIC', {})
         alembic_config.setdefault('script_location', 'db/migrations')
+
+        self.configure(Migrate.configure_alembic_template_directory)
 
         super().init_app(app, db=db,
                          directory=alembic_config.get('script_location'),
                          **app.config.get('ALEMBIC_CONTEXT', {}))
+
+    @staticmethod
+    def configure_alembic_template_directory(config: Config):
+        bundle_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        template_dir = os.path.join(bundle_root, 'alembic', 'templates')
+        setattr(config, 'get_template_directory', lambda: template_dir)
+        return config
