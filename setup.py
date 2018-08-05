fbd27695a211e275ee4b470c7a8bd50a70d608ce
Brian Cappello <briancappello@gmail.com>
2018-08-04 21:11:50 -0600

    add sphinx-rtd-theme to dev requirements

--- setup.py
+++ setup.py
@@ -49,8 +49,9 @@ setup(
             'pytest',
             'pytest-flask',
             'sphinx',
-            'sphinx-click',
             'sphinx-autobuild',
+            'sphinx-click',
+            'sphinx-rtd-theme',
             'tox',
         ],
         'mail': [
