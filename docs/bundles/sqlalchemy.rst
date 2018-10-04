3e7405a680b3bf5d13a3abf6277993cfd99684da
Brian Cappello <briancappello@gmail.com>
2018-10-04 14:58:19 -0600

    begin documenting sqlalchemy bundle usage

--- docs/bundles/sqlalchemy.rst
+++ docs/bundles/sqlalchemy.rst
@@ -40,6 +40,60 @@ Config
    :members:
    :noindex:
 
+Usage
+^^^^^
+
+Usage of the SQLAlchemy bundle starts with an import:
+
+.. code:: python
+
+   from flask_unchained.bundles.sqlalchemy import db
+
+From there, usage is extremely similar to stock Flask SQLAlchemy, and aside from a few minor gotchyas, you should just be able to copy your models and they should work (please file a bug report if this doesn't work!). The gotchyas you should be aware of are:
+
+- the automatic table naming has slightly different behavior if the model class name has sequential upper-case characters
+- any models defined in not-app-bundles must declare themselves as ``class Meta: lazy_mapped = True``
+- you must use ``back_populates`` instead of ``backref`` in ``relationship`` declarations (it may be possible to implement support for backrefs, but honestly, using ``back_populates`` is more Pythonic anyway, because Zen of Python)
+- many-to-many join tables must be declared as model classes
+
+Furthermore, models in SQLAlchemy Unchained include three columns by default:
+
+.. list-table::
+   :header-rows: 1
+   * - column name
+     - description
+   * - ``id``
+     - the primary key
+   * - ``created_at``
+     - a timestamp of when the model got inserted into the database
+   * - ``updated_at``
+     - a timestamp of the last time the model was updated in the database
+
+These are customizable by declaring meta options. For example to disable timestamping and to rename the primary key column to ``pk``, it would look like this:
+
+.. code:: python
+
+   from flask_unchained.bundles.sqlalchemy import db
+
+
+   class Foo(db.Model):
+       class Meta:
+           pk = 'pk'
+           created_at = None
+           updated_at = None
+
+The SQLAlchemy bundle supports `the same meta options as stock SQLAlchemy Unchained <https://github.com/briancappello/sqlalchemy-unchained#included-meta-options>`_, and adds two more:
+
+.. list-table::
+   :header-rows: 1
+
+   * - meta option name
+     - description
+   * - relationships
+     - This is an automatically determined meta option, and is used for determining whether or not a model has the same relationships as its base model. This is useful when you want to override a model from a bundle but change its relationships. The code that determines this is rather experimental, and may not do the right thing. Please report any bugs you come across!
+   * - mv_for
+     - Used for specifying the name of the model a :attr:`~flask_unchained.bundles.sqlalchemy.SQLAlchemy.MaterializedView` is for.
+
 Commands
 ^^^^^^^^
 
--- flask_unchained/bundles/sqlalchemy/config.py
+++ flask_unchained/bundles/sqlalchemy/config.py
@@ -2,10 +2,87 @@ from .alembic.migrations import render_migration_item
 
 
 class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """
+    The default configuration options for the SQLAlchemy Bundle.
+    """
 
     db_file = 'db/dev.sqlite'  # relative path to PROJECT_ROOT/db/dev.sqlite
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_file}'
+    """
+    The database URI that should be used for the connection. See the
+    `SQLAlchemy Dialects documentation <https://docs.sqlalchemy.org/en/latest/dialects/>`_
+    for more info.
+    """
+
+    SQLALCHEMY_ECHO = False
+    """
+    If set to ``True`` SQLAlchemy will log all the statements issued to stderr which can
+    be useful for debugging.
+    """
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """
+    If set to ``True``, Flask-SQLAlchemy will track modifications of objects and emit
+    signals. The default is ``None``, which enables tracking but issues a warning that
+    it will be disabled by default in the future. This requires extra memory and should
+    be disabled if not needed.
+    """
+
+    SQLALCHEMY_RECORD_QUERIES = None
+    """
+    Can be used to explicitly disable or enable query recording. Query recording
+    automatically happens in debug or testing mode. See
+    :func:`~flask_sqlalchemy.get_debug_queries` for more information.
+    """
+
+    SQLALCHEMY_BINDS = None
+    """
+    A dictionary that maps bind keys to SQLAlchemy connection URIs.
+    """
+
+    SQLALCHEMY_NATIVE_UNICODE = None
+    """
+    Can be used to explicitly disable native unicode support. This is required for some
+    database adapters (like PostgreSQL on some Ubuntu versions) when used with improper
+    database defaults that specify encoding-less databases.
+    """
+
+    SQLALCHEMY_POOL_SIZE = None
+    """
+    The size of the database pool. Defaults to the engine’s default (usually 5).
+    """
+
+    SQLALCHEMY_POOL_TIMEOUT = None
+    """
+    Specifies the connection timeout in seconds for the pool.
+    """
+
+    SQLALCHEMY_POOL_RECYCLE = None
+    """
+    Number of seconds after which a connection is automatically recycled. This is
+    required for MySQL, which removes connections after 8 hours idle by default.
+    Note that Flask-SQLAlchemy automatically sets this to 2 hours if MySQL is used.
+
+    Certain database backends may impose different inactive connection timeouts, which
+    interferes with Flask-SQLAlchemy’s connection pooling.
+
+    By default, MariaDB is configured to have a 600 second timeout. This often surfaces
+    hard to debug, production environment only exceptions like ``2013: Lost connection to
+    MySQL server`` during query.
+
+    If you are using a backend (or a pre-configured database-as-a-service) with a lower
+    connection timeout, it is recommended that you set :attr:`SQLALCHEMY_POOL_RECYCLE`
+    to a value less than your backend’s timeout.
+    """
+
+    SQLALCHEMY_MAX_OVERFLOW = None
+    """
+    Controls the number of connections that can be created after the pool reached its
+    maximum size. When those additional connections are returned to the pool, they are
+    disconnected and discarded.
+    """
+
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False
 
     PY_YAML_FIXTURES_DIR = 'db/fixtures'
 
