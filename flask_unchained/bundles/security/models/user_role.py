cd6d955d98134c75138bd74bd2db14704747cf65
Brian Cappello <briancappello@gmail.com>
2018-11-17 21:05:51 -0700

    fix UserRole repr

--- flask_unchained/bundles/security/models/user_role.py
+++ flask_unchained/bundles/security/models/user_role.py
@@ -7,14 +7,13 @@ class UserRole(db.Model):
     """
     class Meta:
         lazy_mapped = True
+        repr = ('user_id', 'role_id')
 
     user_id = db.foreign_key('User', primary_key=True)
     user = db.relationship('User', back_populates='user_roles')
 
     role_id = db.foreign_key('Role', primary_key=True)
     role = db.relationship('Role', back_populates='role_users')
-
-    __repr_props__ = ('user_id', 'role_id')
 
     def __init__(self, user=None, role=None, **kwargs):
         super().__init__(**kwargs)
