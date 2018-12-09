dc66faa11211fa0fcbb5c53fdd3fd9d86cac8da4
Brian Cappello <briancappello@gmail.com>
2018-12-09 16:31:45 -0700

    do not assume the name of the session model if using sqlalchemy with flask-session

--- flask_unchained/bundles/session/hooks/register_session_model_hook.py
+++ flask_unchained/bundles/session/hooks/register_session_model_hook.py
@@ -4,21 +4,23 @@ from flask_unchained import AppFactoryHook
 
 class RegisterSessionModelHook(AppFactoryHook):
     """
-    If using ``SESSION_SQLALCHEMY``, register the ``Session`` model with
+    If using the ``sqlalchemy`` ``SESSION_TYPE``, registers the ``Session`` model with
     the SQLAlchemy Bundle.
     """
 
     bundle_module_name = None
+    name = 'register_session_model'
     run_after = ['init_extensions', 'models']
 
     def run_hook(self, app: Flask, bundles):
+        self.app = app
         if app.config.SESSION_TYPE != 'sqlalchemy':
             return
 
         Session = app.session_interface.sql_session_model
-        self.unchained.sqlalchemy_bundle.models['Session'] = Session
+        self.unchained.sqlalchemy_bundle.models[Session.__name__] = Session
 
     def update_shell_context(self, ctx: dict):
         if ('sqlalchemy_bundle' in self.unchained.bundles
-                and 'Session' in self.unchained.sqlalchemy_bundle.models):
+                and self.app.config.SESSION_TYPE == 'sqlalchemy'):
             ctx.update(self.unchained.sqlalchemy_bundle.models)
