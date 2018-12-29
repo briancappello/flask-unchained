3dadf04482f906ca1a0acbafb727355395e628e4
Brian Cappello <briancappello@gmail.com>
2018-12-28 18:28:52 -0700

    better organize security config options

--- flask_unchained/bundles/security/config.py
+++ flask_unchained/bundles/security/config.py
@@ -14,43 +14,70 @@ from .forms import (
 from .models import AnonymousUser
 
 
-class Config(BundleConfig):
+class AuthenticationConfigMixin(BundleConfig):
     """
-    Default configuration settings for the Security Bundle.
-    """
-
-    SECURITY_TOKEN_AUTHENTICATION_KEY = 'auth_token'
-    """
-    Specifies the query string parameter to read when using token authentication.
+    Config options for logging in and out.
     """
 
-    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
+    SECURITY_LOGIN_FORM = LoginForm
     """
-    Specifies the HTTP header to read when using token authentication.
-    """
-
-    SECURITY_TOKEN_MAX_AGE = None
-    """
-    Specifies the number of seconds before an authentication token expires.
-    Defaults to None, meaning the token never expires.
+    The form class to use for the login view.
     """
 
-    SECURITY_ANONYMOUS_USER = AnonymousUser
+    SECURITY_DEFAULT_REMEMBER_ME = False
     """
-    Class to use for representing anonymous users.
-    """
-
-    SECURITY_UNAUTHORIZED_CALLBACK = lambda: abort(HTTPStatus.UNAUTHORIZED)
-    """
-    This callback gets called when authorization fails. By default we abort with
-    an HTTP status code of 401 (UNAUTHORIZED).
+    Whether or not the login form should default to checking the
+    "Remember me?" option.
     """
 
-    # make datetimes timezone-aware by default
-    SECURITY_DATETIME_FACTORY = lambda: datetime.now(timezone.utc)
+    SECURITY_USER_IDENTITY_ATTRIBUTES = ['email']  # FIXME-identity
     """
-    Factory function to use when creating new dates. By default we use
-    ``datetime.now(timezone.utc)`` to create a timezone-aware datetime.
+    List of attributes on the user model that can used for logging in with.
+    Each must be unique.
+    """
+
+    SECURITY_POST_LOGIN_REDIRECT_ENDPOINT = '/'
+    """
+    The endpoint or url to redirect to after a successful login.
+    """
+
+    SECURITY_POST_LOGOUT_REDIRECT_ENDPOINT = '/'
+    """
+    The endpoint or url to redirect to after a user logs out.
+    """
+
+
+class ChangePasswordConfigMixin(BundleConfig):
+    """
+    Config options for changing passwords
+    """
+
+    SECURITY_CHANGEABLE = False
+    """
+    Whether or not to enable change password functionality.
+    """
+
+    SECURITY_CHANGE_PASSWORD_FORM = ChangePasswordForm
+    """
+    Form class to use for the change password view.
+    """
+
+    SECURITY_POST_CHANGE_REDIRECT_ENDPOINT = None
+    """
+    Endpoint or url to redirect to after the user changes their password.
+    """
+
+    SECURITY_SEND_PASSWORD_CHANGED_EMAIL = \
+        'mail_bundle' in BundleConfig.current_app.unchained.bundles
+    """
+    Whether or not to send the user an email when their password has been changed.
+    Defaults to True, and it's strongly recommended to leave this option enabled.
+    """
+
+
+class EncryptionConfigMixin(BundleConfig):
+    """
+    Config options for encryption hashing.
     """
 
     SECURITY_PASSWORD_SALT = 'security-password-salt'
@@ -102,121 +129,12 @@ class Config(BundleConfig):
     List of deprecated algorithms for creating and validating tokens.
     """
 
-    # login / logout
-    # ==============
-    SECURITY_LOGIN_FORM = LoginForm
+
+class ForgotPasswordConfigMixin(BundleConfig):
     """
-    The form class to use for the login view.
+    Config options for recovering forgotten passwords
     """
 
-    SECURITY_DEFAULT_REMEMBER_ME = False
-    """
-    Whether or not the login form should default to checking the
-    "Remember me?" option.
-    """
-
-    SECURITY_USER_IDENTITY_ATTRIBUTES = ['email']  # FIXME-identity
-    """
-    List of attributes on the user model that can used for logging in with.
-    Each must be unique.
-    """
-
-    SECURITY_POST_LOGIN_REDIRECT_ENDPOINT = '/'
-    """
-    The endpoint or url to redirect to after a successful login.
-    """
-
-    SECURITY_POST_LOGOUT_REDIRECT_ENDPOINT = '/'
-    """
-    The endpoint or url to redirect to after a user logs out.
-    """
-
-    # registration
-    # ============
-    SECURITY_REGISTERABLE = False
-    """
-    Whether or not to enable registration.
-    """
-
-    SECURITY_REGISTER_FORM = RegisterForm
-    """
-    The form class to use for the register view.
-    """
-
-    SECURITY_POST_REGISTER_REDIRECT_ENDPOINT = None
-    """
-    The endpoint or url to redirect to after a user completes the
-    registration form.
-    """
-
-    SECURITY_SEND_REGISTER_EMAIL = \
-        'mail_bundle' in BundleConfig.current_app.unchained.bundles
-    """
-    Whether or not send a welcome email after a user completes the
-    registration form.
-    """
-
-    # email confirmation required functionality
-    # =========================================
-    SECURITY_CONFIRMABLE = False
-    """
-    Whether or not to enable required email confirmation for new users.
-    """
-
-    SECURITY_SEND_CONFIRMATION_FORM = SendConfirmationForm
-    """
-    Form class to use for the (re)send confirmation email form.
-    """
-
-    SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
-    """
-    Allow users to login without confirming their email first. (This option
-    only applies when :attr:`SECURITY_CONFIRMABLE` is True.)
-    """
-
-    SECURITY_CONFIRM_EMAIL_WITHIN = '5 days'
-    """
-    How long to wait until considering the token in confirmation emails to
-    be expired.
-    """
-
-    SECURITY_POST_CONFIRM_REDIRECT_ENDPOINT = None
-    """
-    Endpoint or url to redirect to after the user confirms their email.
-    Defaults to :attr:`SECURITY_POST_LOGIN_REDIRECT_ENDPOINT`.
-    """
-
-    SECURITY_CONFIRM_ERROR_REDIRECT_ENDPOINT = None
-    """
-    Endpoint to redirect to if there's an error confirming the user's email.
-    """
-
-    # change password functionality
-    # =============================
-    SECURITY_CHANGEABLE = False
-    """
-    Whether or not to enable change password functionality.
-    """
-
-    SECURITY_CHANGE_PASSWORD_FORM = ChangePasswordForm
-    """
-    Form class to use for the change password view.
-    """
-
-    SECURITY_POST_CHANGE_REDIRECT_ENDPOINT = None
-    """
-    Endpoint or url to redirect to after the user changes their password.
-    """
-
-    SECURITY_SEND_PASSWORD_CHANGED_EMAIL = \
-        'mail_bundle' in BundleConfig.current_app.unchained.bundles
-    """
-    Whether or not to send the user an email when their password has been changed.
-    Defaults to True, and it's strongly recommended to leave this option enabled.
-    """
-
-    # forgot password functionality
-    # =============================
     SECURITY_RECOVERABLE = False
     """
     Whether or not to enable forgot password functionality.
@@ -227,8 +145,9 @@ class Config(BundleConfig):
     Form class to use for the forgot password form.
     """
 
-    # reset password
+    # reset password (when the user clicks the link from the email sent by forgot pw)
     # --------------
+
     SECURITY_RESET_PASSWORD_FORM = ResetPasswordForm
     """
     Form class to use for the reset password form.
@@ -266,6 +185,123 @@ class Config(BundleConfig):
     """
     Whether or not to send the user an email when their password has been reset.
     Defaults to True, and it's strongly recommended to leave this option enabled.
+    """
+
+
+class RegistrationConfigMixin(BundleConfig):
+    """
+    Config options for user registration
+    """
+
+    SECURITY_REGISTERABLE = False
+    """
+    Whether or not to enable registration.
+    """
+
+    SECURITY_REGISTER_FORM = RegisterForm
+    """
+    The form class to use for the register view.
+    """
+
+    SECURITY_POST_REGISTER_REDIRECT_ENDPOINT = None
+    """
+    The endpoint or url to redirect to after a user completes the
+    registration form.
+    """
+
+    SECURITY_SEND_REGISTER_EMAIL = \
+        'mail_bundle' in BundleConfig.current_app.unchained.bundles
+    """
+    Whether or not send a welcome email after a user completes the
+    registration form.
+    """
+
+    # email confirmation options
+    # --------------------------
+
+    SECURITY_CONFIRMABLE = False
+    """
+    Whether or not to enable required email confirmation for new users.
+    """
+
+    SECURITY_SEND_CONFIRMATION_FORM = SendConfirmationForm
+    """
+    Form class to use for the (re)send confirmation email form.
+    """
+
+    SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
+    """
+    Allow users to login without confirming their email first. (This option
+    only applies when :attr:`SECURITY_CONFIRMABLE` is True.)
+    """
+
+    SECURITY_CONFIRM_EMAIL_WITHIN = '5 days'
+    """
+    How long to wait until considering the token in confirmation emails to
+    be expired.
+    """
+
+    SECURITY_POST_CONFIRM_REDIRECT_ENDPOINT = None
+    """
+    Endpoint or url to redirect to after the user confirms their email.
+    Defaults to :attr:`SECURITY_POST_LOGIN_REDIRECT_ENDPOINT`.
+    """
+
+    SECURITY_CONFIRM_ERROR_REDIRECT_ENDPOINT = None
+    """
+    Endpoint to redirect to if there's an error confirming the user's email.
+    """
+
+
+class TokenConfigMixin(BundleConfig):
+    """
+    Config options for token authentication.
+    """
+
+    SECURITY_TOKEN_AUTHENTICATION_KEY = 'auth_token'
+    """
+    Specifies the query string parameter to read when using token authentication.
+    """
+
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
+    """
+    Specifies the HTTP header to read when using token authentication.
+    """
+
+    SECURITY_TOKEN_MAX_AGE = None
+    """
+    Specifies the number of seconds before an authentication token expires.
+    Defaults to None, meaning the token never expires.
+    """
+
+
+class Config(AuthenticationConfigMixin,
+             ChangePasswordConfigMixin,
+             EncryptionConfigMixin,
+             ForgotPasswordConfigMixin,
+             RegistrationConfigMixin,
+             TokenConfigMixin,
+             BundleConfig):
+    """
+    Default configuration settings for the Security Bundle.
+    """
+
+    SECURITY_ANONYMOUS_USER = AnonymousUser
+    """
+    Class to use for representing anonymous users.
+    """
+
+    SECURITY_UNAUTHORIZED_CALLBACK = lambda: abort(HTTPStatus.UNAUTHORIZED)
+    """
+    This callback gets called when authorization fails. By default we abort with
+    an HTTP status code of 401 (UNAUTHORIZED).
+    """
+
+    # make datetimes timezone-aware by default
+    SECURITY_DATETIME_FACTORY = lambda: datetime.now(timezone.utc)
+    """
+    Factory function to use when creating new dates. By default we use
+    ``datetime.now(timezone.utc)`` to create a timezone-aware datetime.
     """
 
 
