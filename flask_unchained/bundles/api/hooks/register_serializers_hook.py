6d01b9469249cc10bc056d0d3fbc0b95c1f3cbd5
Brian Cappello <briancappello@gmail.com>
2019-09-26 18:33:27 -0600

    remove missed line from marshmallow 2->3 migration

--- flask_unchained/bundles/api/hooks/register_serializers_hook.py
+++ flask_unchained/bundles/api/hooks/register_serializers_hook.py
@@ -1,11 +1,7 @@
-from flask import Flask
-from flask_unchained import AppFactoryHook
-from flask_unchained.string_utils import title_case
+from typing import *
 
-try:
-    from marshmallow import class_registry
-except ImportError:
-    from py_meta_utils import OptionalClass as class_registry
+from flask_unchained import AppFactoryHook, FlaskUnchained
+from flask_unchained.string_utils import title_case
 
 from ..model_serializer import ModelSerializer
 from ..extensions import api
@@ -20,13 +16,16 @@ class RegisterSerializersHook(AppFactoryHook):
     bundle_module_names = ['serializers']
     run_after = ['models']
 
-    def process_objects(self, app: Flask, objects):
-        for name, serializer in objects.items():
+    def process_objects(self,
+                        app: FlaskUnchained,
+                        serializers: Dict[str, Type[ModelSerializer]],
+                        ) -> None:
+        for name, serializer in serializers.items():
             self.bundle.serializers[name] = serializer
-            class_registry.register(name, serializer)
 
             model = serializer.Meta.model
             model_name = model if isinstance(model, str) else model.__name__
+
             kind = getattr(serializer, '__kind__', 'all')
             if kind == 'all':
                 self.bundle.serializers_by_model[model_name] = serializer
@@ -36,10 +35,10 @@ class RegisterSerializersHook(AppFactoryHook):
             elif kind == 'many':
                 self.bundle.many_by_model[model_name] = serializer
 
-    def type_check(self, obj):
+    def type_check(self, obj: Any) -> bool:
         if not isinstance(obj, type):
             return False
         return issubclass(obj, ModelSerializer) and obj != ModelSerializer
 
-    def update_shell_context(self, ctx: dict):
+    def update_shell_context(self, ctx: Dict[str, Any]) -> None:
         ctx.update(self.bundle.serializers)
