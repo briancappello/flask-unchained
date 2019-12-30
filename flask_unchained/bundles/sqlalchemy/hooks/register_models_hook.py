8d8012fd6334f73faf09b199aa8314c44415a717
Brian Cappello <briancappello@gmail.com>
2019-12-29 20:51:51 -0700

    remove unnecessary use of di

--- flask_unchained/bundles/sqlalchemy/hooks/register_models_hook.py
+++ flask_unchained/bundles/sqlalchemy/hooks/register_models_hook.py
@@ -1,12 +1,10 @@
 import sys
 
-from flask_unchained import AppFactoryHook, FlaskUnchained, unchained, injectable
+from flask_unchained import AppFactoryHook, FlaskUnchained
 from flask_unchained.constants import TEST
 from typing import *
 
 from ..base_model import BaseModel
-from ..extensions import SQLAlchemyUnchained
-from ..forms import ModelForm
 from ..model_registry import UnchainedModelRegistry
 
 
@@ -20,13 +18,7 @@ class RegisterModelsHook(AppFactoryHook):
     run_after = ['register_extensions']
     run_before = ['configure_app', 'init_extensions', 'services']
 
-    # skipcq: PYL-W0221 (parameters mismatch in overridden method)
-    @unchained.inject('db')
-    def process_objects(self,
-                        app: FlaskUnchained,
-                        model_forms: Dict[str, Type[ModelForm]],
-                        db: SQLAlchemyUnchained = injectable,
-                        ) -> None:
+    def process_objects(self, app: FlaskUnchained, objects: Dict[str, Any]) -> None:
         # this hook is responsible for discovering models, which happens by
         # importing each bundle's models module. the metaclasses of models
         # register themselves with the model registry, and the model registry
