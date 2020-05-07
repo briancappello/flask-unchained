451e0ea8f1cf2e7811576d4857cdc46597f3e057
Brian Cappello <briancappello@gmail.com>
2020-05-06 18:22:08 -0600

    bump remaining optional deps

--- setup.py
+++ setup.py
@@ -22,20 +22,19 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         'blinker>=1.4',
-        'click>=7.0',
-        'flask>=1.1.1',
+        'click>=7.1.2',
+        'flask>=1.1.2',
         'flask_babelex>=0.9.4',
         'flask-wtf>=0.14.3',
         'py-meta-utils>=0.7.6',
         'pyterminalsize>=0.1.0',
-        'networkx>=2.1',
+        'networkx>=2.4',
         'werkzeug>=1.0.1',
         'wtforms>=2.3.1',
     ],
     extras_require={
         'admin': [
-            'flask-admin>=1.5.3',
-            'sqlalchemy<1.3',
+            'flask-admin>=1.5.6',
         ],
         'api': [
             'apispec>=3.3.0',
@@ -44,7 +43,7 @@ setup(
             'marshmallow-sqlalchemy>=0.23.0',
         ],
         'celery': [
-            'celery>=4.2.1',
+            'celery>=4.4.2',
             'dill>=0.2.8.2',
         ],
         'dev': [
@@ -58,22 +57,25 @@ setup(
             'tox>=3.5.2',
         ],
         'docs': [
-            'IPython>=7.1.1',
-            'PyQt5>=5.11.2',
-            'qtconsole>=4.4.0',
+            'IPython>=7.14.0',
+            'PyQt5>=5.14.2',
+            'qtconsole>=4.7.3',
             'sphinx>=1.8.1',
             'sphinx-autobuild>=0.7.1',
             'sphinx-click>=1.4.0',
             'sphinx-rtd-theme>=0.4.2',
         ],
         'graphene': [
+            'graphql-core>=2.3.1,<3',
+            'graphql-relay>=2.0.1,<3',
+            'graphql-server-core>=1.2.0,<2',
             'flask-graphql>=2.0',
-            'graphene>=2.1.3',
-            'graphene-sqlalchemy>=2.1.0',
+            'graphene>=2.1.8,<3',
+            'graphene-sqlalchemy>=2.2.2,<3',
         ],
         'mail': [
-            'beautifulsoup4>=4.6.3',
-            'lxml>=4.2.4',
+            'beautifulsoup4>=4.9.0',
+            'lxml>=4.5.0',
         ],
         'oauth': [
             'Flask-OAuthlib>=0.9.5',
@@ -90,7 +92,7 @@ setup(
             'flask-session>=0.3.1',
         ],
         'sqlalchemy': [
-            'flask-migrate>=2.2.1',
+            'flask-migrate>=2.5.3',
             'flask-sqlalchemy-unchained>=0.7.3',
             'sqlalchemy-unchained>=0.10.0',
         ],
@@ -105,6 +107,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points='''
         [console_scripts]
