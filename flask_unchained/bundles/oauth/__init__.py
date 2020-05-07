944abde37e68b2a9464b5ab7cd94879b91c7f9a9
Brian Cappello <briancappello@gmail.com>
2020-05-07 15:40:26 -0600

    monkeypatch deprecated flask-oauthlib to work with werkzeug 1.x

--- flask_unchained/bundles/oauth/__init__.py
+++ flask_unchained/bundles/oauth/__init__.py
@@ -1,3 +1,15 @@
+# monkeypatch for using flask-oauthlib with werkzeug 1.x
+import werkzeug
+from werkzeug.http import parse_options_header
+from werkzeug.urls import url_decode, url_encode, url_quote
+from werkzeug.utils import cached_property
+setattr(werkzeug, 'parse_options_header', parse_options_header)
+setattr(werkzeug, 'url_decode', url_decode)
+setattr(werkzeug, 'url_encode', url_encode)
+setattr(werkzeug, 'url_quote', url_quote)
+setattr(werkzeug, 'cached_property', cached_property)
+# end monkeypatch
+
 from flask_unchained import Bundle
 
 from .services import OAuthService
