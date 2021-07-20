5410d82789e76b9502167c7c6c9c515d5ce2bba9
Brian Cappello <briancappello@gmail.com>
2021-07-20 10:28:15 -0600

    update app bundle code template

--- flask_unchained/_code_templates/project/app/__init__.py
+++ flask_unchained/_code_templates/project/app/__init__.py
@@ -2,8 +2,7 @@ from flask_unchained import AppBundle, FlaskUnchained, generate_csrf
 
 
 class App(AppBundle):
-    @classmethod
-    def after_init_app(cls, app: FlaskUnchained):
+    def after_init_app(self, app: FlaskUnchained):
         app.jinja_env.add_extension('jinja2_time.TimeExtension')
 
         @app.after_request
